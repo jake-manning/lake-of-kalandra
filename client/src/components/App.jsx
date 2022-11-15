c991e344caec5298182eaa251fbcb0bb1549a007
jake-manning <jcmanning92@gmail.com>
2022-11-15 10:34:48 -0800

    Map tablet state as divs in Tablet component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Tablet from './Tablet';
 
 const App = () => {
+  const [solution, setSolution] = useState([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]);
+  const [tablet, setTablet] = useState([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]);
+
+  $(document).on('click', '.tablet .row .tile', function () {
+    if($(this).hasClass('active')){
+      $(this).removeClass('active');
+    }
+    else{
+      $(this).addClass('active');
+    }
+  });
+
   return (
     <div>
       <h1>Goodbye World</h1>
-      <Tablet />
+      <Tablet tablet={tablet}/>
     </div>
   )
 };
--- client/src/components/Tablet.jsx
+++ client/src/components/Tablet.jsx
@@ -1,45 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Table = () => {
+const Tablet = ({ tablet }) => {
+  // let html = '';
+  // for (let i = 0; i < 5; i++) {
+  //   html += '<div class="row">';
+  //   for (let j = 0; j < 5; j++) {
+  //     if (tablet[5 * i + j] === 1) {
+  //       html += `<div class="tile" id="${5 * i + j + 1}"></div>`;
+  //     } else if (tablet[5 * i + j] === 0) {
+  //       html += `<div class="tile" id="${5 * i + j + 1}"></div>`;
+  //     }
+  //   }
+  //   html += '</div>';
+  // }
+
+  // dangerouslySetInnerHTML={{__html: html}}
   return (
-    <div className="table">
-      <div className="row">
-        <div className="tile" id="1"></div>
-        <div className="tile" id="2"></div>
-        <div className="tile" id="3"></div>
-        <div className="tile" id="4"></div>
-        <div className="tile" id="5"></div>
-      </div>
-      <div className="row">
-        <div className="tile" id="6"></div>
-        <div className="tile" id="7"></div>
-        <div className="tile" id="8"></div>
-        <div className="tile" id="9"></div>
-        <div className="tile" id="10"></div>
-      </div>
-      <div className="row">
-        <div className="tile" id="11"></div>
-        <div className="tile" id="12"></div>
-        <div className="tile" id="13"></div>
-        <div className="tile" id="14"></div>
-        <div className="tile" id="15"></div>
-      </div>
-      <div className="row">
-        <div className="tile" id="16"></div>
-        <div className="tile" id="17"></div>
-        <div className="tile" id="18"></div>
-        <div className="tile" id="19"></div>
-        <div className="tile" id="20"></div>
-      </div>
-      <div className="row">
-        <div className="tile" id="21"></div>
-        <div className="tile" id="22"></div>
-        <div className="tile" id="23"></div>
-        <div className="tile" id="24"></div>
-        <div className="tile" id="25"></div>
-      </div>
+    <div className="tablet">
+      {tablet.map((row, rowIndex) => {
+        return (
+          <div className="row">
+            {row.map((tile, tileIndex) => {
+              if (!tile) {
+                return <div className="tile" id={`${5 * rowIndex + tileIndex + 1}`}></div>
+              }
+              return <div className="tile active" id={`${5 * rowIndex + tileIndex + 1}`}></div>
+            })}
+          </div>
+      )})}
     </div>
   )
 }
 
-export default Table;
+export default Tablet;
